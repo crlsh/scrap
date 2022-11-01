ea40a85985f1f6ff046f3fcc9c3f90461da5e09e
crlsh <crlshgimenez@gmail.com>
2022-11-01 15:35:54 -0300

    carrefour3

---
+++ carreforu3.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+productos = [
+    {
+        'nombre': 'leche clasis sachet 1L 3%',
+        'url': 'https://www.carrefour.com.ar/leche-entera-fresca-la-serenisima-sachet-11802/p'
+        'selector':'<span class="lyracons-carrefourarg-product-price-1-x-currencyContainer"><span>',
+    },
+    {
+        'nombre': '',
+        'url': '',
+    },
+]
