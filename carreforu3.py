fc8ca7117daa1206636fc91109781862d1f0dad5
crlsh <crlshgimenez@gmail.com>
2022-11-01 18:58:40 -0300

    varios

--- carreforu3.py
+++ carreforu3.py
@@ -8,47 +8,108 @@ headers = {
 }
 
 # armamos el listado con los productos que vamos a recabar informacion
-productos = [
-    {
-        'nombre':
-        'Harina de trigo 0000 ultrarefinada Pureza 1 kg',
-        'url':
-        'https://www.carrefour.com.ar/harina-de-trigo-0000-ultrarefinada-pureza-1-kg/p',
-        'selector':
-        '<span class="lyracons-carrefourarg-product-price-1-x-currencyContainer">',
-    },
-    {
-        'nombre':
-        'Fideos mostacholes rayados Matarazzo 500 g.',
-        'url':
-        'https://www.carrefour.com.ar/fideos-mostacholes-rayados-matarazzo-500-g/p',
-        'selector':
-        '<span class="lyracons-carrefourarg-product-price-1-x-currencyContainer">',
-    },
-    {
-        'nombre':
-        'Arroz parboil Gallo Oro caja 1 kg.',
-        'url':
-        'https://www.carrefour.com.ar/arroz-parboil-gallo-oro-caja-1-kg-37559/p',
-        'selector':
-        '<span class="lyracons-carrefourarg-product-price-1-x-currencyContainer">',
-    },
-    {
-        'nombre':
-        'Leche entera fresca La Serenísima sachet',
-        'url':
-        'https://www.carrefour.com.ar/leche-entera-fresca-la-serenisima-sachet-11802/p',
-        'selector':
-        '<span class="lyracons-carrefourarg-product-price-1-x-currencyContainer">',
-    },
-    {
-        'nombre':
-        'Manteca con crema de leche La Serenísima paquete 200 g.',
-        'url':
-        'https://www.carrefour.com.ar/manteca-con-crema-de-leche-la-serenisima-paquete-200-g-699641-699641/p',
-        'selector':
-        '"lyracons-carrefourarg-product-price-1-x-currencyContainer"',
-    },
+productos = [{
+    'super':
+    "Carrefour",
+    'categoria':
+    'harina',
+    'nombre':
+    'Harina de trigo 0000 ultrarefinada Pureza 1 kg',
+    'url':
+    'https://www.carrefour.com.ar/harina-de-trigo-0000-ultrarefinada-pureza-1-kg/p',
+    'selector':
+    'span',
+    'clase':
+    'lyracons-carrefourarg-product-price-1-x-currencyInteger',
+}, {
+    'super':
+    "Carrefour",
+    'categoria':
+    'fideos',
+    'nombre':
+    'Fideos mostacholes rayados Matarazzo 500 g.',
+    'url':
+    'https://www.carrefour.com.ar/fideos-mostacholes-rayados-matarazzo-500-g/p',
+    'selector':
+    'span',
+    'clase':
+    'lyracons-carrefourarg-product-price-1-x-currencyInteger',
+}, {
+    'super':
+    "Carrefour",
+    'categoria':
+    'arroz',
+    'nombre':
+    'Arroz parboil Gallo Oro caja 1 kg.',
+    'url':
+    'https://www.carrefour.com.ar/arroz-parboil-gallo-oro-caja-1-kg-37559/p',
+     'selector':
+    'span',
+    'clase':
+    'lyracons-carrefourarg-product-price-1-x-currencyInteger',
+}, {
+    'super':
+    "Carrefour",
+    'categoria':
+    'leche',
+    'nombre':
+    'Leche entera fresca La Serenísima sachet',
+    'url':
+    'https://www.carrefour.com.ar/leche-entera-fresca-la-serenisima-sachet-11802/p',
+    'selector':
+    'span',
+    'clase':
+    'lyracons-carrefourarg-product-price-1-x-currencyInteger',
+}, {
+    'super':
+    "Carrefour",
+    'categoria':
+    'manteca',
+    'nombre':
+    'Manteca con crema de leche La Serenísima paquete 200 g.',
+    'url':
+    'https://www.carrefour.com.ar/manteca-con-crema-de-leche-la-serenisima-paquete-200-g-699641-699641/p',
+    'selector':
+    'span',
+    'clase':
+    'lyracons-carrefourarg-product-price-1-x-currencyInteger',
+}, {
+    'super':
+    "Carrefour",
+    'categoria':
+    'arroz maximo',
+    'nombre':
+    'Arroz parboil Máximo bolsa 500 g',
+    'url':
+    'https://www.carrefour.com.ar/arroz-parboil-maximo-bolsa-500-g/p',
+    'selector':
+    'span',
+    'clase':
+    'lyracons-carrefourarg-product-price-1-x-currencyInteger',
+}, {
+    'super': "la reina",
+    'categoria': 'leche',
+    'nombre': 'Manteca con crema de leche La Serenísima paquete 200 g.',
+    'url':
+    'https://www.lareinaonline.com.ar/productosdet.asp?Pr=7793940448003&P=2',
+    'selector': 'div',
+    'clase': 'izq',
+}, {
+    'super': "la reina",
+    'categoria': 'arroz',
+    'nombre': 'Arroz Oro Parboil E/Bolsa Gallo 500 Gr',
+    'url':
+    'https://www.lareinaonline.com.ar/productosdet.asp?Pr=7790070411839&P=1',
+    'selector': 'div',
+    'clase': 'izq',
+}
+
+
+
+
+
+
+
 ]
 
 # Por cada producto:
@@ -56,13 +117,18 @@ for producto in productos:
 
     # Por cada producto, obtenemos la info de la pagina con el request.
     #  El timeout es para esperar a que cargue la pagina. para que los datos cargados posteriormente con css mediante before o javascript esten disponibles disposnibles
-    url=str(producto['url'])
+    url = str(producto['url'])
     page = requests.get(url, headers=headers, timeout=(1000, 1500))
     soup = BeautifulSoup(page.content, 'html.parser')
-    
-    element = soup.find("span", class_="lyracons-carrefourarg-product-price-1-x-currencyInteger")
-    precio=element.text
-    print (url, precio)
-  
 
-    
+    # element = soup.find("span", class_="lyracons-carrefourarg-product-price-1-x-currencyInteger")
+    precio = soup.find((producto['selector']), class_=producto['clase'])
+
+    print(producto['super'], producto['categoria'], precio)
+
+#   estrucutra de datos csv?
+
+#   producto
+#   super
+#   fecha
+#   valor
