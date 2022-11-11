924537c7dd9989ea3b7a6ef914cf6bb3d4a88449
crlsh <crlshgimenez@gmail.com>
2022-11-11 19:07:29 -0300

    changes

--- backup pruebas/prueba3.py
+++ backup pruebas/prueba3.py
@@ -1,33 +1,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#leer datos del archivo json y crear dataframe
-data = pd.read_json('https://raw.githubusercontent.com/crlsh/scrap/main/datos.json')
+# leer datos del archivo json y crear dataframe
+data = pd.read_json(
+    'https://raw.githubusercontent.com/crlsh/scrap/main/datos.json')
 df = pd.DataFrame(data)
 
-#limpiar el dataframe (vacios y duplicados)
+# limpiar el dataframe (vacios y duplicados)
 
 df.dropna(inplace=True)
 df.drop_duplicates(inplace=True)
 
-#imprimir ultimos 5 datos del dataframe
-print ("ultimos 5 datos del df \n", df.tail(n=5))
+# imprimir ultimos 5 datos del dataframe
+print("ultimos 5 datos del df \n", df.tail(n=5))
 
-#comparar valores 
-## en TODOS los datos 
+# comparar valores
+# en TODOS los datos
 max_price = df['precio'].max()
-print ("Precio mas alto : ", max_price)
+print("Precio mas alto : ", max_price)
 
 
 min_price = df['precio'].min()
-print ("Precio mas bajo : ", min_price)
+print("Precio mas bajo : ", min_price)
 
-## precio promedio del aceite en todos los super
+# precio promedio del aceite en todos los super
 
 mean1 = df['precio'].mean()
-print ("precio promedio aceite :", mean1)
+print("precio promedio aceite :", mean1)
 
-print ("\n")
+print("\n")
 
 # cant de items por tabla
 groupby_count1 = df.groupby(['super']).count()
@@ -35,11 +36,11 @@ print("- items por tabla \n", groupby_count1, "\n")
 
 
 # Agrupar por producto...
-prods= df.groupby(['producto'])
+prods = df.groupby(['producto'])
 for name, group in prods:
-      superProd=prods.apply(lambda x: x)
-      print(superProd)
-      print(name)
-      print(group)
+    superProd = prods.apply(lambda x: x)
+    print(superProd)
+    print(name)
+    print(group)
 
-     
+
