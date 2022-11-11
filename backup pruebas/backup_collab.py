73b8f2d62e33c7784b859b3901ac050b14719019
crlsh <crlshgimenez@gmail.com>
2022-11-11 18:25:05 -0300

    backup collab

---
+++ backup pruebas/backup_collab.py
@@ -0,0 +1,32 @@
+
+# buscar haciendo queries
+#ejemplo=df.query('super =="Carrefour" and producto =="arroz"')
+#print ("Evolucion precio arroz en carrefour \n" ,ejemplo)
+
+#precio = ejemplo['precio']
+
+
+#corregir formato fecha
+#ejemplo["fecha"] = [float(str(i).replace("-", "")) for i in ejemplo["fecha"]]
+#fecha= ejemplo['fecha']
+#print ("\n",fecha,"\n")
+
+
+
+#ejemplo2=df.query('super =="Dino" and producto =="arroz"')
+#print ("Evolucion precio arroz en Dino \n" ,ejemplo2)
+
+#precio2 = ejemplo2['precio']
+
+
+#corregir formato fecha
+#ejemplo2["fecha"] = [float(str(i).replace("-", "")) for i in ejemplo2["fecha"]]
+#fecha2= ejemplo2['fecha']
+#print ("\n",fecha2,"\n")
+
+
+#print("--- grafico precio arroz Carrefour \n")
+#plt.plot (fecha, precio, linestyle='solid', label="Carrefour")
+
+#print("--- grafico precio arroz Dino \n")
+#plt.plot (fecha2, precio2, linestyle='solid', label="Dino")
